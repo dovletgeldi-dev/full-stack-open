191b459eafa2cefc52fb83f879bfb248554ca6f6
Dovlet <dovlethelegend@gmail.com>
2023-08-30 17:07:42 +0800

    finshed 2.9: phonebook step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,30 +3,32 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "213231321" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterQuery, setFilterQuery] = useState("");
 
   console.log(persons);
 
   const addName = (event) => {
     event.preventDefault();
 
-    const duplicateName = persons.some((person) => person.name === newName);
-
-    if (duplicateName) {
+    if (persons.some((person) => person.name === newName)) {
       return alert(`${newName} is already added to phonebook`);
+    } else {
+      const nameObject = {
+        name: newName,
+        number: newNumber,
+      };
+
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+      setNewNumber("");
     }
-
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-    };
-
-    setPersons(persons.concat(nameObject));
-    setNewName("");
-    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
@@ -37,10 +39,19 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterQuery = (event) => {
+    setFilterQuery(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filterQuery} onChange={handleFilterQuery} />
+      </div>
       <form onSubmit={addName}>
+        <h2>Add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -52,15 +63,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map((person) => (
-          <p key={person.name}>
+
+      {persons
+        .filter((person) => person.name.toLowerCase().includes(filterQuery))
+        .map((person) => (
+          <p key={person.id}>
             {person.name} {person.number}
           </p>
         ))}
-        <p>debug: {newName}</p>
-        <p>debug: {newNumber}</p>
-      </div>
     </div>
   );
 };
