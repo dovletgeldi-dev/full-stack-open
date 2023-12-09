e4be2d7ae8fa2dd9b05e64b36cc4a4f514d08ce7
Dovletgeldi Myratgeldiyev <dovletgeldi.m@tuneprotect.com>
2023-12-10 01:32:45 +0800

    finished 3.4: Phonebook backend step4

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const date = new Date().toString();
 console.log(date);
 
@@ -48,6 +50,13 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
 });
 
 const PORT = 3001;
