b9da1daa42e5407a6e8f761048f65083fa0b0cbf
Dovlet <dovlethelegend@gmail.com>
2024-04-06 21:27:35 +0500

    finished exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div
+      style={{ border: "2px solid black", margin: "1rem 0", padding: "1rem" }}
+    >
+      {blog.title} {blog.author}{" "}
+      <button onClick={toggleVisibility}>{visible ? "hide" : "view"}</button>
+      {!visible ? (
+        <></>
+      ) : (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
